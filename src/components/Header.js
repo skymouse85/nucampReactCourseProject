c8531c371a1db832bc9d544dc2b167d7ea7db535
Mark Clifford <markmclifford@gmail.com>
2022-11-04 21:24:04 -0700

    header navs

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,45 @@
-import { Navbar, NavbarBrand } from "reactstrap";
+import { useState } from 'react';
+import {
+    Navbar,
+    NavbarBrand,
+    Collapse,
+    NavbarToggler,
+    Nav,
+    NavItem
+} from "reactstrap";
+import { NavLink } from 'react-router-dom';
 import NucampLogo from '../app/assets/img/logo.png'
 
 const Header = () => {
     return (<Navbar dark color='primary' sticky='top' expand='md'>
-        <NavbarBrand href='/'>
-            <img src={NucampLogo} alt='nucamp logo' />
+        <NavbarBrand className='ms-5' href='/'>
+            <img src={NucampLogo} alt='nucamp logo' className='float-start' />
+            <h1 className='mt-1' >NuCamp</h1>
         </NavbarBrand>
-    </Navbar>)
+        <Nav className='ms-auto' navbar>
+            <NavItem>
+                <NavLink className='nav-link' to='/'>
+                    <i className='fa fa-home fa-lg'>Home</i>
+                </NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink className='nav-link' to='/directory'>
+                    <i className='fa fa-list fa-lg'>Directory</i>
+                </NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink className='nav-link' to='/about'>
+                    <i className='fa fa-info fa-lg'>About</i>
+                </NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink className='nav-link' to='/contact'>
+                    <i className='fa fa-address-card fa-lg'>Contact</i>
+                </NavLink>
+            </NavItem>
+        </Nav>
+    </Navbar>
+    )
 }
 
 export default Header;
