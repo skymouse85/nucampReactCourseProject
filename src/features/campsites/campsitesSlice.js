31687256fe6a4cc8a3d666b6cd9a0c8085ff9f75
Mark Clifford <markmclifford@gmail.com>
2022-11-04 20:28:12 -0700

    hooks round one

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -5,6 +5,9 @@ export const selectAllCampsites = () => {
     return CAMPSITES;
 }
 
-export const selectRandomCampsite = () => {
-    return CAMPSITES[Math.floor(Math.random() * CAMPSITES.length)];
+export const selectCampsiteById = (id) => {
+    return CAMPSITES.find((campsite) => campsite.id === id)
 }
+// export const selectRandomCampsite = () => {
+//     return CAMPSITES[Math.floor(Math.random() * CAMPSITES.length)];
+// }
